015db98a27b1536c8105ee1d277c9c3d6c7c7e10
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-17 17:36:57 +0700

    fix

--- fun_4/main_1.py
+++ fun_4/main_1.py
@@ -1,11 +1,11 @@
-arr = []
 
 def multiplication_table(range_x,range_y):
     for i in range(1,range_x+1):
         for j in range(1,range_y+1):
             result = i*j
-            arr.append(result)
-        print(arr)
-        arr.clear()
-            
+            if(j == range_y):
+                print(f"{result:4}")
+            else:
+                print(f"{result:4}",end=" ")
+
 multiplication_table(25,12)
