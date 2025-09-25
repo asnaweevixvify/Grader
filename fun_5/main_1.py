f9473fa77dbca9e76dffe44059f0a99e21501d48
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-25 20:01:54 +0700

    list comprehension

--- fun_5/main_1.py
+++ fun_5/main_1.py
@@ -1,12 +1,8 @@
 def extract_words(text):
     new_text = text.split()
-    arr = []
-    for i in new_text:
-        if(len(i) >= 4):
-            arr.append(i)
-        else:
-            continue
-    return arr
+    status = [x for x in new_text if len(x) >= 4]
+    return status
+    
 print(extract_words("Don't judge a book by its cover."))
 print(extract_words("All that glitters is not gold."))
 print(extract_words("The multinational corporation's decentralization strategy required comprehensive reorganization of their compartmentalized departmentalization systems across intercontinental subsidiaries." ))
